738b4359ed7c626537722428a3fca90c5f713ea1
liqihua <iamliqihua@qq.com>
2019-03-07 09:44:05 +0800

    Initial commit

---
+++ nacos-config-demo-spring-cloud/src/main/java/com/liqihua/demo/nacos/Run.java
@@ -0,0 +1,16 @@
+package com.liqihua.demo.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liqihua
+ * @since 2019/3/6
+ */
+@SpringBootApplication
+public class Run {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+    }
+}
